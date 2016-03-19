bd42956364c1d873d8b5c1f46317bbfc695484ea
Saymon Pires <piresaymon@gmail.com>
2016-03-19 15:23:18 -0300

    novo index.html

--- manage.py
+++ manage.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
-from django.core.management import execute_manager
 
+import settings
 import imp
+import os
+import sys
+
+try:
+    import settings  # Assumed to be in the same directory.
+except ImportError:
+    import sys
+    sys.stderr.write(
+        """Error: Can't find the file 'settings.py' in the directory containing
+%r. It appears you've customized things.\n
+You'll have to run django-admin.py, passing it your settings module.
+(If the file settings.py does indeed exist, it's causing an ImportError.)\n"""
+        % __file__)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+    # fix known PyPy bug (see https://bugs.pypy.org/issue1116)
+    import gc
+    gc.collect()
+    gc.collect()
+
+
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
-
-import settings
-
-if __name__ == "__main__":
-    execute_manager(settings)
