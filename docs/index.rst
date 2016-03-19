355666b3b612923a6c589e1a1da30ebc966c71bf
Saymon Pires <piresaymon@gmail.com>
2016-03-19 17:19:09 -0300

    index novo

---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+.. Iskrillex documentation master file, created by
+   sphinx-quickstart on Mon Mar 03 17:33:12 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Iskrillex's documentation!
+=====================================
+This is my introduction to this project.
+
+Requirements:
+
+My Iskrillex project depends
+
+Contents:gvfgvfc
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ static/js/elevator.js
@@ -0,0 +1,200 @@
+/*!
+ * Elevator.js
+ *
+ * MIT licensed
+ * Copyright (C) 2015 Tim Holman, http://tholman.com
+ */
+
+/*********************************************
+ * Elevator.js
+ *********************************************/
+
+var Elevator = function(options) {
+
+    'use strict';
+
+    // Elements
+    var body = null;
+
+    // Scroll vars
+    var animation = null;
+    var duration = null; // ms
+    var customDuration = false;
+    var startTime = null;
+    var startPosition = null;
+    var elevating = false;
+
+    var mainAudio;
+    var endAudio;
+
+    var that = this;
+    /**
+     * Utils
+     */
+
+    // Thanks Mr Penner - http://robertpenner.com/easing/
+    function easeInOutQuad( t, b, c, d ) {
+        t /= d / 2;
+        if ( t < 1 ) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * ( t * ( t -2 ) - 1 ) + b;
+    };
+
+    function extendParameters(options, defaults){
+        for( var option in defaults ){
+            var t = options[option] === undefined && typeof option !== "function";
+            if(t){
+                options[option] = defaults[option];
+            }
+        }
+        return options;
+    }
+
+    /**
+     * Main
+     */
+
+    // Time is passed through requestAnimationFrame, what a world!
+    function animateLoop( time ) {
+        if ( !startTime ) {
+            startTime = time;
+        }
+
+        var timeSoFar = time - startTime;
+        var easedPosition = easeInOutQuad(timeSoFar, startPosition, -startPosition, duration);
+
+        window.scrollTo(0, easedPosition);
+
+        if( timeSoFar < duration ) {
+            animation = requestAnimationFrame(animateLoop);
+        } else {
+            animationFinished();
+        }
+   };
+
+//            ELEVATE!
+//              /
+//         ____
+//       .'    '=====<0
+//       |======|
+//       |======|
+//       [IIIIII[\--()
+//       |_______|
+//       C O O O D
+//      C O  O  O D
+//     C  O  O  O  D
+//     C__O__O__O__D
+//    [_____________]
+    this.elevate = function() {
+
+        if( elevating ) {
+            return;
+        }
+
+        elevating = true;
+        startPosition = (document.documentElement.scrollTop || body.scrollTop);
+
+        // No custom duration set, so we travel at pixels per millisecond. (0.75px per ms)
+        if( !customDuration ) {
+            duration = (startPosition * 3);
+        }
+
+        requestAnimationFrame( animateLoop );
+
+        // Start music!
+        if( mainAudio ) {
+            mainAudio.play();
+        }
+    }
+
+    function resetPositions() {
+        startTime = null;
+        startPosition = null;
+        elevating = false;
+    }
+
+    function animationFinished() {
+
+        resetPositions();
+
+        // Stop music!
+        if( mainAudio ) {
+            mainAudio.pause();
+            mainAudio.currentTime = 0;
+        }
+
+        if( endAudio ) {
+            endAudio.play();
+        }
+    }
+
+    function onWindowBlur() {
+
+        // If animating, go straight to the top. And play no more music.
+        if( elevating ) {
+
+            cancelAnimationFrame( animation );
+            resetPositions();
+
+            if( mainAudio ) {
+                mainAudio.pause();
+                mainAudio.currentTime = 0;
+            }
+
+            window.scrollTo(0, 0);
+        }
+    }
+
+    //@TODO: Does this need tap bindings too?
+    function bindElevateToElement( element ) {
+        element.addEventListener('click', that.elevate, false);
+    }
+
+    function init( _options ) {
+
+        // Bind to element click event, if need be.
+        body = document.body;
+
+        var defaults = {
+            duration: undefined,
+            mainAudio: false,
+            endAudio: false,
+            preloadAudio: true,
+            loopAudio: true,
+        };
+
+        _options = extendParameters(_options, defaults);
+
+        if( _options.element ) {
+            bindElevateToElement( _options.element );
+        }
+
+        if( _options.duration ) {
+            customDuration = true;
+            duration = _options.duration;
+        }
+
+        window.addEventListener('blur', onWindowBlur, false);
+
+        // If the browser doesn't support audio, stop here!
+        if ( !window.Audio ) {
+            return;
+        }
+
+        if( _options.mainAudio ) {
+            mainAudio = new Audio( _options.mainAudio );
+            mainAudio.setAttribute( 'preload', _options.preloadAudio );
+            mainAudio.setAttribute( 'loop', _options.loopAudio );
+        }
+
+        if( _options.endAudio ) {
+            endAudio = new Audio( _options.endAudio );
+            endAudio.setAttribute( 'preload', 'true' );
+        }
+
+    }
+
+    init(options);
+
+};
+
